30f074f7721c41d2a935a9e315596cf114b87ee2
Nick Markwell <nick@duckinator.net>
2013-04-18 20:52:25 -0400

    Number incrementally by total instead of per-dir.

--- fetch-from-why.rb
+++ fetch-from-why.rb
@@ -2,6 +2,7 @@
 
 require 'open-uri'
 require 'time'
+require 'fileutils'
 
 $old    = nil
 
@@ -16,20 +17,17 @@ def fetch_all
     date = Time.parse(date)
     date_dir = date.strftime('%Y/%m/%d')
 
-    number = 1
     files = Dir[File.join(File.dirname(__FILE__), 'PCL', date_dir, '*')]
     if files.any? {|x| x.split('_', 2)[-1].upcase == name.upcase }
       puts "Skipping #{name.upcase} (already exists)."
       next
     end
 
-    old = files.map do |x|
-      x.split('/').last.split('_', 2).first
-    end.map(&:to_i).max
-    number = old + 1 if old.is_a?(Numeric)
+    number = `find PCL -type f | sed 's/.*\\\///' | cut -d '_' -f 1 | sort -u`.split("\n").last.to_i + 1
 
     local_file = "#{number.to_s.rjust(3, '0')}_#{name.upcase}"
     local_dir  = File.join(File.dirname(__FILE__), 'PCL', date_dir)
+    FileUtils.mkdir_p(local_dir)
     full_local_file = File.join(local_dir, local_file)
 
     print "Saving #{root + url} to #{full_local_file}... "
